8d5b65f1c7eff940d426b3977ba7801a5565c874
burnwood1911 <burnwood1911@gmail.com>
2022-03-04 16:29:45 +0300

    new commit

---
+++ src/spotify.js
@@ -0,0 +1,26 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const clientId = "a64e9f5ee72c46c3a1ef08cdfd09f92b";
+const redirectUri = "https://react-spotify-clone-a860b.web.app";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
